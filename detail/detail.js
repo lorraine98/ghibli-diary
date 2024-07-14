489ea4d09eac648fed95abca3fd463552ac902cb
hyosung <hyosung0425@naver.com>
2024-07-14 22:59:10 +0900

    Render diary data

--- detail/detail.js
+++ detail/detail.js
@@ -1,17 +1,52 @@
 import { getDiaries } from "../api/diaries.js";
 import { QueryParamKeys } from "../common/routes.js";
 
-// const {
-//   content,
-//   createdAt,
-//   evaluation,
-//   id: diaryId,
-//   movie: { id: movieId, posterUrl, rating, releaseDate, title },
-// } = data.diary;
+const getEvaluationText = (evaluation) => {
+  switch (evaluation) {
+    case "excellent":
+      return "👍 최고에요";
+    case "average":
+      return "💪 무난해요";
+    case "poor":
+      return "👎 별로예요";
+    default:
+      return "평가없음";
+  }
+};
+
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
 
 const renderDiary = (diary) => {
-  console.log(diary);
   const movieTitle = document.querySelector(".movie-title");
+  movieTitle.textContent = diary.movie.title;
+
+  const poster = document.querySelector(".poster");
+  poster.src = diary.movie.posterUrl;
+
+  const releaseDate = document.querySelector(".release-date");
+
+  const releaseDateStr = formatDate(new Date(diary.movie.releaseDate));
+  releaseDate.textContent = releaseDateStr;
+
+  const rating = document.querySelector(".rating");
+  rating.textContent = `⭐️ ${diary.movie.rating}`;
+
+  const createdAt = document.querySelector(".created-at");
+  const createdDateStr = formatDate(new Date(diary.createdAt));
+  createdAt.textContent = createdDateStr;
+
+  const evaluation = document.querySelector(".evaluation");
+  const evaluationText = getEvaluationText(diary.evaluation);
+  evaluation.textContent = evaluationText;
+
+  const content = document.querySelector(".content");
+  content.textContent = diary.content;
 };
 
 const fetchDiary = async () => {
