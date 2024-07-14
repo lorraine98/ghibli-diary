55c70e122b908a475443b793d631141ac663f77b
hyosung <hyosung0425@naver.com>
2024-07-14 22:59:10 +0900

    Replace innerHTML to create DOM method

--- home/home.js
+++ home/home.js
@@ -7,35 +7,44 @@ let isDiaryWritten = queryParams.get(QueryParamKeys.isDiaryWritten) === "true";
 const renderMovies = (movies) => {
   const moviesList = document.querySelector(".movies-list");
   if (movies.length === 0) {
-    moviesList.innerHTML = isDiaryWritten
-      ? `
-        <li>아직 기록한 작품이 없어요</li>
-      `
-      : `
-        <li>모든 작품을 기록했어요</li>
-      `;
+    moviesList.textContent = "";
+    const li = document.createElement("li");
+
+    isDiaryWritten
+      ? (li.textContent = "아직 기록한 작품이 없어요")
+      : (li.textContent = "모든 작품을 기록했어요");
+
+    moviesList.appendChild(li);
     return;
   }
 
-  moviesList.innerHTML = movies
-    .map((movie) => {
-      const { id, title, posterUrl, diaryId } = movie;
+  moviesList.textContent = "";
 
-      // todo : session storage에 제목 저장하기
-      const href = diaryId
-        ? `${Routes.detail}?movie-id=${id}&diary-id=${diaryId}`
-        : `${Routes.write}?movie-id=${id}`;
+  movies.forEach((movie) => {
+    const { id, title, posterUrl, diaryId } = movie;
 
-      return `
-          <li>
-            <a href="${href}">
-              <img width="200px" src="${posterUrl}" alt="${title}" />
-              <p>${title}</p>
-            </a>
-          </li>
-        `;
-    })
-    .join("");
+    // todo : session storage에 title 저장하기
+    const href = diaryId
+      ? `${Routes.detail}?movie-id=${id}&diary-id=${diaryId}`
+      : `${Routes.write}?movie-id=${id}`;
+
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.setAttribute("href", href);
+
+    const img = document.createElement("img");
+    img.setAttribute("src", posterUrl);
+    img.setAttribute("alt", title);
+    img.setAttribute("width", "200px");
+
+    const p = document.createElement("p");
+    p.textContent = title;
+
+    a.appendChild(img);
+    a.appendChild(p);
+    li.appendChild(a);
+    moviesList.appendChild(li);
+  });
 };
 
 const pushWithNewUrl = () => {
