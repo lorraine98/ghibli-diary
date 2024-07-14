49f97a4c769d306765c99d92f35803705caac345
hyosung <hyosung0425@naver.com>
2024-07-14 22:59:10 +0900

    Make default tab select and decide where to push

--- home/home.js
+++ home/home.js
@@ -1,44 +1,50 @@
 import { fetchMovies } from "../api/movies.js";
 
-const addTabClickEvent = () => {
-  let isDiaryWritten = false;
+let isDiaryWritten = false;
 
-  const renderMovies = (movies) => {
-    const moviesList = document.querySelector(".movies-list");
-    if (movies.length === 0) {
-      moviesList.innerHTML = isDiaryWritten
-        ? `
+const renderMovies = (movies) => {
+  const moviesList = document.querySelector(".movies-list");
+  if (movies.length === 0) {
+    moviesList.innerHTML = isDiaryWritten
+      ? `
         <li>아직 기록한 작품이 없어요</li>
       `
-        : `
+      : `
         <li>모든 작품을 기록했어요</li>
       `;
-      return;
-    }
+    return;
+  }
 
-    moviesList.innerHTML = movies.map((movie) => {
-      const { id, title, posterUrl, diaryId } = movie;
+  moviesList.innerHTML = movies.map((movie) => {
+    const { id, title, posterUrl, diaryId } = movie;
 
-      return `
-          <li data-id="${id}">
-            <a href="/detail?movie-id=${id}&diary-id=${diaryId}">
-              <img src="${posterUrl}" alt="${title}"/>
+    // todo : session storage에 제목 저장하기
+    const href = diaryId
+      ? `/detail?movie-id=${id}&diary-id=${diaryId}`
+      : `/write?movie-id=${id}`;
+
+    return `
+          <li>
+            <a href="${href}">
+              <img width="200px" crossorigin="anonymous" src="${posterUrl}" alt="${title}" />
+              <p>${title}</p>
             </a>
           </li>
         `;
-    });
-  };
+  });
+};
 
-  const fetchMoviesByDiaryWritten = async () => {
-    const data = await fetchMovies(isDiaryWritten);
-    renderMovies(data.movies);
-  };
+const pushWithNewUrl = () => {
+  const newUrl = `${window.location.pathname}?isDiaryWritten=${isDiaryWritten}`;
+  window.history.pushState({ path: newUrl }, "", newUrl);
+};
 
-  const pushWithNewUrl = () => {
-    const newUrl = `${window.location.pathname}?isDiaryWritten=${isDiaryWritten}`;
-    window.history.pushState({ path: newUrl }, "", newUrl);
-  };
+const fetchMoviesByDiaryWritten = async () => {
+  const data = await fetchMovies(isDiaryWritten);
+  renderMovies(data.movies);
+};
 
+const addTabClickEvent = () => {
   const handleTabClick = (event) => {
     isDiaryWritten = event.target.getAttribute("data-diary-written");
 
@@ -54,6 +60,8 @@ const addTabClickEvent = () => {
 };
 
 const init = () => {
+  pushWithNewUrl();
+  fetchMoviesByDiaryWritten();
   addTabClickEvent();
 };
 
